1667a93696c2ffa46427362ad1062471d5044d4a
zilto <thierry.jean.ac@gmail.com>
2022-08-16 21:37:35 -0400

    refactored blogs

--- app/main.py
+++ app/main.py
@@ -45,7 +45,6 @@ def temp(request: Request):
     return templates.TemplateResponse("temp.html", context)
 
 
-
 @app.get("/projects/", status_code=200, response_class=HTMLResponse)
 def projects(request: Request):
     projects = [{"name": "cribbage", "html_url": "https://github.com/zilto/cribbage", "created_at": "2022-02-21T02:48:50Z"}, {"name": "ExcelQueryFormatter", "html_url": "https://github.com/zilto/ExcelQueryFormatter", "description": "Executable containing a python + pandas script to format Excel files", "created_at": "2020-08-05T00:21:19Z"}, {"name": "Honor-Thesis_friends-family", "html_url": "https://github.com/zilto/Honor-Thesis_friends-family", "description": "Predict next-day stress and happiness level using smartphone sensor data", "created_at": "2021-12-22T03:53:08Z"}, {"name": "IFT6390-Comp1", "html_url": "https://github.com/zilto/IFT6390-Comp1", "description": "IFT6390 - Kaggle competition #1 at https://www.kaggle.com/c/ift3395-6390-weatherevents", "created_at": "2021-12-15T15:47:47Z"}, {"name": "IFT6390-Comp2", "html_url": "https://github.com/zilto/IFT6390-Comp2", "description": "IFT6390 - Kaggle competition #2 at https://www.kaggle.com/c/cropharvest-crop-detection", "created_at": "2021-11-21T23:45:17Z"}, {"name": "imgur_archive", "html_url": "https://github.com/zilto/imgur_archive", "description": "Simple Python script to download Imgur content", "created_at": "2021-12-22T00:56:14Z"}, {"name": "IPWS-2021_TDAM", "html_url": "https://github.com/zilto/IPWS-2021_TDAM", "description": "Presentation given to TD Asset Managements representatives by the end of the 11th Industrial Problem Solving Workshop ", "created_at": "2021-12-15T15:59:29Z"}, {"name": "passe-passe", "html_url": "https://github.com/zilto/passe-passe", "description": "CLI tool to encrypt and store credentials", "created_at": "2020-11-02T01:22:47Z"}, {"name": "spotify_webapp", "html_url": "https://github.com/zilto/spotify_webapp", "description": "A small web app to download Spotify content to your device", "created_at": "2022-04-24T23:27:16Z"}, {"name": "streamlit_webapp", "html_url": "https://github.com/zilto/streamlit_webapp", "created_at": "2022-04-10T00:19:13Z"}, {"name": "zilto.github.io", "html_url": "https://github.com/zilto/zilto.github.io", "created_at": "2021-02-03T03:58:45Z"}]
@@ -62,8 +61,8 @@ def project(request: Request):
 
 @app.get("/blogs/", status_code=200, response_class=HTMLResponse)
 def blogs(request: Request):
-    all_blogs = ""
-    context = get_context(request)
+    blogs = [{"title": "The perks of creating dataflows with Hamilton", "filename": "hamilton_blog.md", "publication_date": "2022-08-08"}]
+    context = get_context(request, blogs=blogs)
     return templates.TemplateResponse("blogs.html", context)
 
 
