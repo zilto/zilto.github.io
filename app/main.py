8e1bf2b28fc7bde350f745bf65a065552bcb3dc8
zilto <thierry.jean.ac@gmail.com>
2022-08-16 21:21:57 -0400

    refactored projects

--- app/main.py
+++ app/main.py
@@ -29,56 +29,49 @@ app = get_app()
 
 @app.get("/", status_code=200, response_class=HTMLResponse)
 def home(request: Request):
-    context = {"request": request}
+    context = get_context(request)
     return templates.TemplateResponse("index.html", context)
 
 
 @app.get("/index", status_code=200, response_class=HTMLResponse)
 def index(request: Request):
-    context = {"request": request}
+    context = get_context(request)
     return templates.TemplateResponse("index.html", context)
+
+
+@app.get("/temp", status_code=200, response_class=HTMLResponse)
+def temp(request: Request):
+    context = get_context(request)
+    return templates.TemplateResponse("temp.html", context)
+
 
 
 @app.get("/projects/", status_code=200, response_class=HTMLResponse)
 def projects(request: Request):
-    all_projects = [{"name": "cribbage", "html_url": "https://github.com/zilto/cribbage", "created_at": "2022-02-21T02:48:50Z"}, {"name": "ExcelQueryFormatter", "html_url": "https://github.com/zilto/ExcelQueryFormatter", "description": "Executable containing a python + pandas script to format Excel files", "created_at": "2020-08-05T00:21:19Z"}, {"name": "hamilton", "html_url": "https://github.com/zilto/hamilton", "description": "A scalable general purpose micro-framework for defining dataflows. You can use it to create dataframes, numpy matrices, python objects, ML models, etc.", "created_at": "2022-07-24T21:49:50Z"}, {"name": "Honor-Thesis_friends-family", "html_url": "https://github.com/zilto/Honor-Thesis_friends-family", "description": "Predict next-day stress and happiness level using smartphone sensor data", "created_at": "2021-12-22T03:53:08Z"}, {"name": "IFT6390-Comp1", "html_url": "https://github.com/zilto/IFT6390-Comp1", "description": "IFT6390 - Kaggle competition #1 at https://www.kaggle.com/c/ift3395-6390-weatherevents", "created_at": "2021-12-15T15:47:47Z"}, {"name": "IFT6390-Comp2", "html_url": "https://github.com/zilto/IFT6390-Comp2", "description": "IFT6390 - Kaggle competition #2 at https://www.kaggle.com/c/cropharvest-crop-detection", "created_at": "2021-11-21T23:45:17Z"}, {"name": "imgur_archive", "html_url": "https://github.com/zilto/imgur_archive", "description": "Simple Python script to download Imgur content", "created_at": "2021-12-22T00:56:14Z"}, {"name": "IPWS-2021_TDAM", "html_url": "https://github.com/zilto/IPWS-2021_TDAM", "description": "Presentation given to TD Asset Managements representatives by the end of the 11th Industrial Problem Solving Workshop ", "created_at": "2021-12-15T15:59:29Z"}, {"name": "passe-passe", "html_url": "https://github.com/zilto/passe-passe", "description": "CLI tool to encrypt and store credentials", "created_at": "2020-11-02T01:22:47Z"}, {"name": "spotify_webapp", "html_url": "https://github.com/zilto/spotify_webapp", "description": "A small web app to download Spotify content to your device", "created_at": "2022-04-24T23:27:16Z"}, {"name": "streamlit_webapp", "html_url": "https://github.com/zilto/streamlit_webapp", "created_at": "2022-04-10T00:19:13Z"}, {"name": "zilto.github.io", "html_url": "https://github.com/zilto/zilto.github.io", "created_at": "2021-02-03T03:58:45Z"}]
-    context = {"request": request, "all_projects": all_projects}
+    projects = [{"name": "cribbage", "html_url": "https://github.com/zilto/cribbage", "created_at": "2022-02-21T02:48:50Z"}, {"name": "ExcelQueryFormatter", "html_url": "https://github.com/zilto/ExcelQueryFormatter", "description": "Executable containing a python + pandas script to format Excel files", "created_at": "2020-08-05T00:21:19Z"}, {"name": "Honor-Thesis_friends-family", "html_url": "https://github.com/zilto/Honor-Thesis_friends-family", "description": "Predict next-day stress and happiness level using smartphone sensor data", "created_at": "2021-12-22T03:53:08Z"}, {"name": "IFT6390-Comp1", "html_url": "https://github.com/zilto/IFT6390-Comp1", "description": "IFT6390 - Kaggle competition #1 at https://www.kaggle.com/c/ift3395-6390-weatherevents", "created_at": "2021-12-15T15:47:47Z"}, {"name": "IFT6390-Comp2", "html_url": "https://github.com/zilto/IFT6390-Comp2", "description": "IFT6390 - Kaggle competition #2 at https://www.kaggle.com/c/cropharvest-crop-detection", "created_at": "2021-11-21T23:45:17Z"}, {"name": "imgur_archive", "html_url": "https://github.com/zilto/imgur_archive", "description": "Simple Python script to download Imgur content", "created_at": "2021-12-22T00:56:14Z"}, {"name": "IPWS-2021_TDAM", "html_url": "https://github.com/zilto/IPWS-2021_TDAM", "description": "Presentation given to TD Asset Managements representatives by the end of the 11th Industrial Problem Solving Workshop ", "created_at": "2021-12-15T15:59:29Z"}, {"name": "passe-passe", "html_url": "https://github.com/zilto/passe-passe", "description": "CLI tool to encrypt and store credentials", "created_at": "2020-11-02T01:22:47Z"}, {"name": "spotify_webapp", "html_url": "https://github.com/zilto/spotify_webapp", "description": "A small web app to download Spotify content to your device", "created_at": "2022-04-24T23:27:16Z"}, {"name": "streamlit_webapp", "html_url": "https://github.com/zilto/streamlit_webapp", "created_at": "2022-04-10T00:19:13Z"}, {"name": "zilto.github.io", "html_url": "https://github.com/zilto/zilto.github.io", "created_at": "2021-02-03T03:58:45Z"}]
+    context = get_context(request, projects=projects)
     return templates.TemplateResponse("projects.html", context)
 
 
 @app.get("/project/{project_id:int}", status_code=200, response_class=HTMLResponse)
 def project(request: Request):
     project = ""
-    context = {"request": Request, "project": project}
+    context = get_context(request, project=project)
     return templates.TemplateResponse("project.html", context)
     
 
 @app.get("/blogs/", status_code=200, response_class=HTMLResponse)
 def blogs(request: Request):
     all_blogs = ""
-    context = {"request": request}
-    return templates.TemplateResponse("blog.html", context)
+    context = get_context(request)
+    return templates.TemplateResponse("blogs.html", context)
 
 
 @app.get("/blog/{blog_title:str}", status_code=200, response_class=HTMLResponse)
-def project(request: Request):
-    project = ""
-    context = {"request": Request, "project": project}
-    return templates.TemplateResponse("project.html", context)
-
-
-# TODO load the blogs from db
-@app.get("/blogpost/", status_code=200, response_class=HTMLResponse)
-def blogpost(request: Request):
-    # blogpost_html = markdown.markdownFromFile(input=f"/blogs/")
-    context = {"request": request}
-    return templates.TemplateResponse("blogpost.html", context)
-
-
-@app.get("/braindump/", status_code=200, response_class=HTMLResponse)
-def braindump(request: Request):
-    context = {"request": request}
-    return templates.TemplateResponse("braindump.html", context)
+def blog(request: Request):
+    blog = ""
+    context = get_context(request, blog=blog)
+    return templates.TemplateResponse("blog.html", context)
 
 
 if __name__ == "__main__":
