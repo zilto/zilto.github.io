a97867f4fd265a0d96f69aa40bbc54ef70bba23f
zilto <tjean@DESKTOP-V6JDCS2>
2024-02-10 12:53:24 -0500

    set url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https:/www.tjean.me',
-  url: "https://zilto.github.io",
+  url: "https://tjean.me",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
